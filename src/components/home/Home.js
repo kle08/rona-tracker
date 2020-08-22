873931d2ef0c465570464b2b3ad75460b9b54925
iamasianman <kevinle1991@gmail.com>
2020-08-21 17:05:13 -0700

    implemented leaflet map

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import { MenuItem, FormControl, Select, Card, CardContent } from '@material-ui/core'
+import React, { useState, useEffect } from 'react';
+import { MenuItem, FormControl, Select, Card, CardContent, } from '@material-ui/core';
 import './Home.css';
 import InfoBox from './InfoBox';
 import Map from './Map';
+import Table from './Table';
+import { sortData } from './util'
+import LineGraph from './LineGraph';
+import "leaflet/dist/leaflet.css";
 
 export default function Home() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([])
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 })
+  const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([])
+
 
   useEffect(() => {
     fetch('https://disease.sh/v3/covid-19/all')
@@ -21,10 +30,14 @@ export default function Home() {
         .then((response) => response.json())
         .then((data) => {
           const countries = data.map(country => ({
+            id: country.countryInfo.id,
             name: country.country,
             value: country.countryInfo.iso2
           }))
-          setCountries(countries)
+          const sortedData = sortData(data);
+          setTableData(sortedData);
+          setCountries(countries);
+          setMapCountries(data);
         })
     }
     getCountriesData()
@@ -41,6 +54,8 @@ export default function Home() {
       .then(data => {
         setCountryInfo(data)
         setCountry(countryCode)
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long])
+        setMapZoom(4)
       })
   }
 
@@ -65,16 +80,17 @@ export default function Home() {
           <InfoBox title='Recovered' cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
           <InfoBox title='Deaths' cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
         </div>
-        <Map />
+        <Map center={mapCenter}
+          zoom={mapZoom}
+          countries={mapCountries} />
       </div>
       <Card className='home__right'>
-        {/* {table} */}
         <CardContent>
           <h3>live cases </h3>
+          <Table countries={tableData} />
           <h3>live new cases </h3>
-
         </CardContent>
-        {/* {graph} */}
+        <LineGraph />
       </Card>
     </div>
   )
